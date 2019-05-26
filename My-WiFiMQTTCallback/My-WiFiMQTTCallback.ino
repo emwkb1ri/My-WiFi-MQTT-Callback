5624c68cbb3ce37e757c8dca8b4f1761e775df08
emwkb1ri <34179331+emwkb1ri@users.noreply.github.com>
2019-05-26 15:02:06 -0400

    Initial changes to use MQTT with NodeMCU - known bug with WiFi #include

--- My-WiFiMQTTCallback/My-WiFiMQTTCallback.ino
+++ My-WiFiMQTTCallback/My-WiFiMQTTCallback.ino
@@ -6,7 +6,7 @@
   When a message is received it prints the message to the serial monitor,
   it uses the callback functionality of the library.
 
-  It also demonstrates how to set the will message, get/set QoS, 
+  It also demonstrates how to set the will message, get/set QoS,
   duplicate and retain values of messages.
 
   The circuit:
@@ -16,27 +16,42 @@
 */
 
 #include <ArduinoMqttClient.h>
-#include <WiFiNINA.h> // for MKR1000 change to: #include <WiFi101.h>
+//#include <WiFiNINA.h> // for MKR1010 boards
+//#include <WiFi101.h>  // for MKR 1000 boards
+#include <ESP8266WiFi.h> // for NodeMCU and ESP8266 ethernet modules
 
 #include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-const char *myHostname = "MKR1010";  // set the hostname of this device
+const char *myHostname = "NodeMCU";  // set the hostname of this device
 byte mac[6];  // the MAC address of your WiFi Module
 IPAddress ip; // the IP address of this board
 
 // To connect with SSL/TLS:
 // 1) Change WiFiClient to WiFiSSLClient.
 // 2) Change port value from 1883 to 8883.
-// 3) Change broker value to a server with a known SSL/TLS root certificate 
+// 3) Change broker value to a server with a known SSL/TLS root certificate
 //    flashed in the WiFi module.
 
-// initial variables for pin IO
+/*
+// initial variables for pin IO on my MKR1010 protoboard
 const int LED = 6; // built in board LED
 const int LED1 = 0;
 const int LED2 = 1;
 const int LED3 = 2;
+*/
+// initial variables for pin IO on my NodeMCU protoboard
+const int LED = 6; // built in board LED
+const int LED1 = 14; // GPIO 14
+const int LED2 = 13; // GPIO 13
+const int LED3 = 5; // GPIO 5
+const int LED4 = 15; // GPIO 15
+const int LED5 = 10; // GPIO 10
+const int LED6 = 4; // GPIO 4
+const int LED7 = 16; // GPIO 16
+const int LED8 = 12; // GPIO 12
+
 const int d1 = 2000; // first flash long on time
 const int d2 = 1000; // off time
 const int d3 = 500;  // second flash short on time
@@ -48,7 +63,7 @@ const char broker[] =  "192.168.1.250";   //  broker host is NR4O-pi1 - otiginal
 int        port     = 1883;
 const char willTopic[] = "BarricAid/will";
 const char inTopic[]   = "BarricAid/test";
-const char outTopic[]  = "BarricAid/out";  
+const char outTopic[]  = "BarricAid/out";
 char msg[128] = "";  //message buffer
 boolean newMsg = false;
 
@@ -56,7 +71,7 @@ const long interval = 10000;
 unsigned long previousMillis = 0;
 boolean glob_flash = false;
 unsigned long cMillis = 0;
-unsigned long p1Millis = 0; 
+unsigned long p1Millis = 0;
 unsigned long p2Millis = 0;
 unsigned long wifiConnectCount = 0;
 unsigned long mqttConnectCount = 0;
@@ -73,15 +88,26 @@ int count = 0;
 
 void setup() {
   // initialize digital pins as an output.
-  
+
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+  pinMode(LED7, OUTPUT);
+  pinMode(LED8, OUTPUT);
 
   digitalWrite(LED1, HIGH);    // turn all the LEDs off to start HIGH = LED OFF
   digitalWrite(LED2, HIGH);
   digitalWrite(LED3, HIGH);
-  
+  digitalWrite(LED4, HIGH);    // turn all the LEDs off to start HIGH = LED OFF
+  digitalWrite(LED5, HIGH);
+  digitalWrite(LED6, HIGH);
+  digitalWrite(LED7, HIGH);    // turn all the LEDs off to start HIGH = LED OFF
+  digitalWrite(LED8, HIGH);
+
+
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -90,7 +116,7 @@ void setup() {
 
   // attempt to connect to WiFi network
   connectToWiFi();
-/*  
+/*
   // attempt to connect to Wifi network:
   Serial.print("Attempting to connect to WPA SSID: ");
   Serial.println(ssid);
@@ -114,8 +140,8 @@ void setup() {
 
   // set a will message, used by the broker when the connection dies unexpectantly
   // you must know the size of the message before hand, and it must be set before connecting
-  
-  String willPayload = "oh no! - MKR1010 stopped responding";
+
+  String willPayload = "oh no! - NodeMCU stopped responding";
   bool willRetain = true;
   int willQos = 1;
 
@@ -166,9 +192,9 @@ void loop() {
 
   if (!mqttClient.connected()){
     Serial.println("MQTT NOT CONNECTED!");
-	mqttConnectCount += 1;  // increment the connection count
+	  mqttConnectCount += 1;  // increment the connection count
     mqttUpTime = millis() - mqttStartTime;
-	mqttCumUpTime += mqttUpTime;
+	  mqttCumUpTime += mqttUpTime;
     mqttAvgUpTimeSec = mqttCumUpTime / (mqttConnectCount - 1) / 1000;
     Serial.print("MQTT Connection Uptime: ");
     Serial.print(mqttUpTime/1000);
@@ -178,48 +204,52 @@ void loop() {
     Serial.println(" seconds");
 
     if (!wifiClient.connected()) {
-	  wifiConnectCount += 1; // increment WiFi connection counter	
+	  wifiConnectCount += 1; // increment WiFi connection counter
 	  Serial.print("WiFi Disconnected...");
 	  Serial.print(wifiConnectCount);
-	  Serial.print("...WiFi Status: ");	
-	  
-	  switch (WiFi.status()) {
-		  case WL_CONNECTION_LOST :
-			Serial.print("CONNECTION_LOST");
-			break;
-		  case WL_DISCONNECTED :
-			Serial.print("DISONNECTED");
-			break;
-		  case WL_CONNECTED :
-			Serial.print("CONNECTED");
-			break;
-		  case WL_NO_SHIELD :
-			Serial.print("NO_SHIELD");
-			break;
-		  case WL_NO_SSID_AVAIL :
-			Serial.print("NO_SSID_AVAIL");
-			break;
-		  case WL_CONNECT_FAILED :
-			Serial.print("CONNECT_FAILED");
-			break;
-		  default :
-			// no matches to the above - must be one 
-			//   WL_AP_CONNECTED, WL_AP_LISTENING, WL_IDLE_STATUS, WL_SCAN_COMPLETED
-			Serial.println("One of 4 other status returns");
-	  }
-	  
-      Serial.println("...attempting reconnect");
-      connectToWiFi();  
+	  Serial.print("...WiFi Status: ");
+
+  	  switch (WiFi.status()) {
+  		  case WL_CONNECTION_LOST :
+  			Serial.print("CONNECTION_LOST");
+        Serial.println("...attempting WiFi reconnect");
+        connectToWiFi();
+  			break;
+  		  case WL_DISCONNECTED :
+  			Serial.print("DISONNECTED");
+        Serial.println("...attempting WiFi reconnect");
+        connectToWiFi();
+  			break;
+  		  case WL_CONNECTED :
+  			Serial.print("CONNECTED");
+  			break;
+  		  case WL_NO_SHIELD :
+  			Serial.print("NO_SHIELD");
+  			break;
+  		  case WL_NO_SSID_AVAIL :
+  			Serial.print("NO_SSID_AVAIL");
+  			break;
+  		  case WL_CONNECT_FAILED :
+  			Serial.print("CONNECT_FAILED");
+        Serial.println("...attempting WiFi reconnect");
+        connectToWiFi();
+  			break;
+  		  default :
+  			// no matches to the above - must be one
+  			//   WL_AP_CONNECTED, WL_AP_LISTENING, WL_IDLE_STATUS, WL_SCAN_COMPLETED
+  			Serial.println("One of 4 other status returns");
+  	  }
     }
+    Serial.println();
     if (MQTT_connect() == true){  // attempt to reconnect to MQTT broker
       mqttStartTime = millis();  // reset the start time if connected
     }
   }
   mqttClient.poll();
   if (mqttConnectCount == 1){ // set the Avg Up Time to the current up time on first connection
-    mqttAvgUpTimeSec = (millis() - mqttStartTime) / 1000; 
+    mqttAvgUpTimeSec = (millis() - mqttStartTime) / 1000;
   }
-  
+
   if (newMsg == true) {
     // check message for a command
     newMsg = false;
@@ -236,11 +266,11 @@ void loop() {
         digitalWrite(LED2, HIGH);    // turn the LED ON by making the voltage LOW
       }
       else if (strcmp(msg, "LED 3 ON") == 0) {
-		statusFlag = false;  
+		statusFlag = false;
         digitalWrite(LED3, LOW);    // turn the LED ON by making the voltage LOW
       }
       else if (strcmp(msg, "LED 3 OFF") == 0) {
-		statusFlag = true;  
+		statusFlag = true;
         digitalWrite(LED3, HIGH);    // turn the LED ON by making the voltage LOW
       }
       else {
@@ -252,9 +282,9 @@ void loop() {
   // avoid having delays in loop, we'll use the strategy from BlinkWithoutDelay
   // see: File -> Examples -> 02.Digital -> BlinkWithoutDelay for more info
   unsigned long currentMillis = millis();
-  
+
   flash3("");    // flash LED 3 as needed
-  
+
   if ((currentMillis - previousMillis >= interval) && statusFlag) {
     // save the last time a message was sent
     previousMillis = currentMillis;
@@ -264,15 +294,15 @@ void loop() {
     payload += "BarricAid Status: ";
     payload += " ";
     payload += count;
-	payload += " Receive Count: ";
-	payload += msgRcvCount;
+	  payload += " Receive Count: ";
+	  payload += msgRcvCount;
     payload += "\nRestart Count: ";
     payload += mqttConnectCount - 1;
     payload += "  Avg Up Time: ";
     payload += days_hrs_mins_secs(mqttAvgUpTimeSec);
-	payload += "\nCurrent Uptime: ";  // add current uptime to message payload
-	payload += days_hrs_mins_secs((millis() - mqttStartTime)/1000); // add current uptime to message payload
-	
+	  payload += "\nCurrent Uptime: ";  // add current uptime to message payload
+	  payload += days_hrs_mins_secs((millis() - mqttStartTime)/1000); // add current uptime to message payload
+
 
     Serial.print("Sending message to topic: ");
     Serial.println(outTopic);
@@ -290,7 +320,7 @@ void loop() {
     mqttClient.endMessage();
 
     Serial.println();
-    
+
     count++;
 
     // toggle the onboard LED on each published message
@@ -299,7 +329,7 @@ void loop() {
     } else {
       ledState = LOW;
     }
-    digitalWrite(LED, ledState);  
+    digitalWrite(LED, ledState);
   }
 }
 
@@ -330,16 +360,16 @@ void onMqttMessage(int messageSize) {
   newMsg = true;
   msgRcvCount += 1;  // increment the received message counter
   Serial.println(msg); // print the message received
-  
+
   Serial.println();
 }
 
 void flash3(String s){
   // used to flash LED3 quickly for 5 seconds when an invalid message is recieved.
-  
+
   const long i = 200;   // 300 mSec timer
   const long j = 5000;  // 5 second timer
-  
+
   if (s == "Start") {
     // initialize the timer count variables if called with "Start"
     Serial.println("Start LED3 flash");
@@ -349,7 +379,7 @@ void flash3(String s){
     p2Millis = cMillis;
     led3State = LOW; //turn on the LED to start
   }
-  cMillis = millis();  
+  cMillis = millis();
   // if glob_flash is true - fast flash at i mSec intervals for j mSec of time
   if (glob_flash && (cMillis - p1Millis >= i)) {
     p1Millis = cMillis;
@@ -367,7 +397,7 @@ void flash3(String s){
   }
   // set the LED with the leState of the variable
   digitalWrite(LED3, led3State);
-  }  
+  }
 }
 
 void connectToWiFi(){
@@ -402,7 +432,7 @@ void connectToWiFi(){
   Serial.print("Firmware Version: ");
   Serial.println(WiFi.firmwareVersion());
 
-  // print the MAC address beginning with byte 5 to byte 0 
+  // print the MAC address beginning with byte 5 to byte 0
   Serial.print("MAC: ");
   for (unsigned int i = 5; i > 0; i--) {
     Serial.print(mac[i],HEX);
@@ -427,7 +457,7 @@ void array_to_string(byte array[], unsigned int len, char buffer[]){
   //      byte array to be converted
   //      length of array
   //      destination char buffer
-  
+
     for (unsigned int i = 0; i < len; i++){
         byte nib1 = (array[i] >> 4) & 0x0F;  // mask the first nibble of data in element i of array
         byte nib2 = (array[i] >> 0) & 0x0F;  // mask the second nibble of data in element i of array
@@ -447,14 +477,14 @@ boolean  MQTT_connect() {
     return true;
   }
 
-  Serial.print("Connecting to MQTT... ");
+  Serial.print("Connecting to MQTT broker... ");
 
-  if(!mqttClient.connect(broker, port)) {
+  while(!mqttClient.connect(broker, port)) {
     Serial.print("MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
     Serial.println("Retrying MQTT connection in 5 seconds...");
     // mqttClient.disconnect();
-    // delay(5000);  // wait 5 seconds
+    delay(5000);  // wait 5 seconds
   }
   Serial.println("MQTT Connected!");
 
@@ -478,7 +508,7 @@ boolean  MQTT_connect() {
   Serial.print("Waiting for messages on topic: ");
   Serial.println(inTopic);
   Serial.println();
-  
+
   return true;
 }
 
@@ -490,21 +520,21 @@ String days_hrs_mins_secs(unsigned int s){
   unsigned int t_hrs = 0;
   unsigned int hrs = 0;
   unsigned int days = 0;
-  String r = ""; 
-  
+  String r = "";
+
   if (s < 1) {
       // no complete seconds have elapsed
       r = "0 Days 0 Hrs 0 Mins 0 Secs";
       return r;
-  } 
-   
+  }
+
   secs = s % 60;    // remaining # of seconds
-  
+
   if (s > 60) {
-    t_mins = (s - secs) / 60; // total minutes 
+    t_mins = (s - secs) / 60; // total minutes
     mins = t_mins % 60;  // remainder of minutes
-  }  
-  if (t_mins > 60) { 
+  }
+  if (t_mins > 60) {
       t_hrs = (t_mins - mins) / 60; // total hours
       hrs = t_hrs % 24; // remainder of hours in days
   }
@@ -520,4 +550,4 @@ String days_hrs_mins_secs(unsigned int s){
   r += secs;
   r += " Secs";
   return r;
-} 
+}
